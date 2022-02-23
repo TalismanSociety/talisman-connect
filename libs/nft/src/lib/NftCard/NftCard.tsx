6f13d026bf8c082cd10950b7f576055cfc83c2da
f00 <julius@talisman.xyz>
2022-02-23 10:11:23 +0800

    feat(nft): add rmrk link on card (#12)

--- libs/nft/src/lib/NftCard/NftCard.tsx
+++ libs/nft/src/lib/NftCard/NftCard.tsx
@@ -1,5 +1,6 @@
 import { NftElement } from '../../types';
 import Card from '../Card/Card';
+import { collectibleUrl } from '../fetchers/rmrk1-fetcher';
 import NftContentType from '../NftContentType/NftContentType';
 import NftDescription from '../NftDescription/NftDescription';
 import NftImage from '../NftImage/NftImage';
@@ -8,15 +9,22 @@ import styles from './NftCard.module.css';
 export function NftCard(props: NftElement) {
   const { nft } = props;
   return (
-    <Card
-      header={<NftImage nft={nft} />}
-      description={
-        <div className={styles['nft-card-description']}>
-          <NftDescription nft={nft} />
-          <NftContentType nft={nft} />
-        </div>
-      }
-    />
+    <a
+      href={collectibleUrl(nft.id)}
+      className={styles['nft-link']}
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      <Card
+        header={<NftImage nft={nft} />}
+        description={
+          <div className={styles['nft-card-description']}>
+            <NftDescription nft={nft} />
+            <NftContentType nft={nft} />
+          </div>
+        }
+      />
+    </a>
   );
 }
 
--- libs/nft/src/lib/fetchers/rmrk1-fetcher.ts
+++ libs/nft/src/lib/fetchers/rmrk1-fetcher.ts
@@ -4,6 +4,10 @@ interface FetcherURL {
   address: string;
 }
 
+export function collectibleUrl(id: string) {
+  return `https://singular.rmrk.app/collectibles/${id}`;
+}
+
 export function fetchUrl({ address }: FetcherURL) {
   const kusamaAddress = encodeAddress(address, 2);
   return `https://singular.rmrk.app/api/rmrk1/account/${kusamaAddress}`;
