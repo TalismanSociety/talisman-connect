4b7ed1829788719ea0b6b7f9910290aec9026f50
Jonathan Dunne <37194960+jonathanpdunne@users.noreply.github.com>
2022-08-26 11:14:49 +1000

    update p-js install URL

--- libs/wallets/src/lib/polkadotjs-wallet/index.ts
+++ libs/wallets/src/lib/polkadotjs-wallet/index.ts
@@ -6,8 +6,7 @@ export class PolkadotjsWallet extends BaseDotsamaWallet {
   title = 'Polkadot.js';
   noExtensionMessage =
     'You can use any Polkadot compatible wallet but we recommend using Talisman';
-  installUrl =
-    'https://chrome.google.com/webstore/detail/polkadot%7Bjs%7D-extension/mopnmbcafieddcagagdcbnhejhlodfdd';
+  installUrl ='https://polkadot.js.org/extension/'
   logo = {
     src: logo,
     alt: 'Polkadotjs Logo',
