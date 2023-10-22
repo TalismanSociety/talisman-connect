60e3b1b96464cef615dcbf8f40ab4c0e542a56c0
Maciej Tarasiuk <maciej.tarasiuk@enginearch.com>
2023-10-22 14:09:35 +0200

    Add Aleph Zero wallet

---
+++ packages/connect-wallets/src/lib/aleph-zero-wallet/index.ts
@@ -0,0 +1,14 @@
+import { BaseDotsamaWallet } from '../base-dotsama-wallet';
+import logo from './AlephZeroLogo.svg';
+
+export class AlephZeroWallet extends BaseDotsamaWallet {
+  extensionName = 'aleph-zero';
+  title = 'Aleph Zero Signer';
+  installUrl = 'https://alephzero.org/signer';
+  noExtensionMessage =
+    'You can use any Polkadot compatible wallet but we recommend using Talisman';
+  logo = {
+    src: logo,
+    alt: 'Aleph Zero Logo',
+  };
+}
