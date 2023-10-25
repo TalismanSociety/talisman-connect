89e3bbf6220c1908a3c28f6bfd05a0f569846674
Chid Gilovitz <chid@talisman.xyz>
2023-10-25 15:24:36 +0800

    fix: removed unneeded deps from test app

--- apps/test-dapp/src/App.tsx
+++ apps/test-dapp/src/App.tsx
@@ -2,30 +2,9 @@ import { useEffect } from "react";
 import "./App.css";
 import { WalletSelect } from "@talismn/connect-components";
 import { PolkadotjsWallet, SubWallet, TalismanWallet, FearlessWallet, EnkryptWallet, PolkaGate, MantaWallet, AlephZeroWallet } from "@talismn/connect-wallets"
-import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
 
 
 function App() {
-
-
-  const Accounts = async () => {
-    // returns an array of all the injected sources
-    // (this needs to be called first, before other requests)
-    const allInjected = await web3Enable('talisman');
-
-    const allExtensions = allInjected.map(({ name, version }) => `${name} ${version}`);
-
-    // returns an array of { address, meta: { name, source } }
-    // meta.source contains the name of the extension that provides this account
-    const allAccounts = await web3Accounts();
-
-    console.log(allAccounts)
-  }
-
-  useEffect(() => {
-    Accounts();
-  }, [])
-
   return (
     <div className="App">
       <WalletSelect
